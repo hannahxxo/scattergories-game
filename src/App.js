861bc7333b2d9df444cb3ba7c7be8e38de619554
hannahxxo <hannahantle@gmail.com>
2021-04-23 10:28:51 -0500

    Initial version of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Letter from './letter/Letter'
+import Timer from './timer/Timer';
+import Categories from './categories/Categories';
+import { useState } from 'react';
 
 function App() {
+  const [hide, setHide] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-Section-One'>
+        <Letter />
+        <Timer setHide={setHide} />
+      </div>
+      <div className='App-Section-Two'>
+        <Categories hide={hide} setHide={setHide} />
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/letter/Letter.js
@@ -0,0 +1,24 @@
+import './Letter.css'
+import { useState } from 'react'
+
+const Letter = () => {
+  const [index, setIndex] = useState(0);
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const changeLetter = () => {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    setIndex(randomIndex);
+  }
+  return (
+    <div className='Letter'>
+      <div className='Letter-Top'>
+        <p>LETTER</p>
+        <button onClick={changeLetter} className='Letter-Top-Btn'>Roll Again</button>
+      </div>
+      <div className='Letter-Display'>
+        <h1 className='Letter-Display-H1'>{letters[index]}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Letter;
